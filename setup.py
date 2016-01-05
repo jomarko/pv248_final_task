7583ec6ece33524ee0e9ebef0b53d3fb83c683e4
jomarko <jomarko@redhat.com>
2016-01-05 14:53:33 +0100

    added sources do dist

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='regexp_server',
       version='1.0',
-      py_modules=['regexp_server']
+      py_modules=['regexp_server'],
+      packages=find_packages()
       )
